f143cd097ba5be098e407c911b2c88af60d09637
Yoimer Roman <ydrf19@gmail.com>
2017-01-15 17:19:01 -0400

    Added Virtual Pin 5 sending a variable called temp every second using the SimpleTimer library from blink

--- Arduino_Serial_USB_NextWells.ino
+++ Arduino_Serial_USB_NextWells.ino
@@ -21,35 +21,35 @@
  *            USB HOWTO: http://tiny.cc/BlynkUSB
  **************************************************************/
 
- 
- /***************************************************************
- * This part of the code was used in a test project under Windows and Arduino UNO with Luis Castillo and Yoimer Román from NextWells company. Venezuela-USA.
- * Instrucctions:
- * - Get on Google Play Store or IOS Store and Download blynk. After installing, sign up.
-   - On your browser from you PC type http://docs.blynk.cc/. Check out the "Intro" section. Read this part carefully.
-   - We will need the blink libraries in our Arduino IDE in order stablish a connection with the Cloud Blynk Service. To do that; get
-      the lastest version of the libraries from here. https://github.com/blynkkk/blynk-library/releases/tag/v0.4.3 (Blynk_Release_v0.4.3.zip)
-      At the time this document was made, the lastest release was v0.4.3. Yours may vary.
-   - In case you've forgotten how to install libraries in Arduino IDE;
-      check this link on "Manual installation" https://www.arduino.cc/en/guide/libraries
-     After installing the Blink libraries, open an empty Scketch from Arduino IDE.
-     Copy and paste the ***NextWells-BLINK-ARDUINO UNO******* code Upload it to the Arduino UNO board.(This code is above)
- 
-   - After succesfully uploading the code,extract the Blynk_Release_v0.4.3.zip files on C:\ directory.
-     This is for running the script which will make us get online with the Cloud Blynk Service.
-   - Run the blynk-ser.bat script. In order to do that , open cmd promt and get on this path C:\Blynk_Release_v0.4.3\libraries\Blynk\scripts>
-     Issue this command : blynk-ser.bat -c COM3 (In your case check your Arduino COM port) and press Enter, Enter , Enter (Yes, 3 times)
-     If there is Internet Connection and everything is OK, you should see something similar to this on the promt console:
 
-     Connecting device at COM3 to blynk-cloud.com:8442...
-     OpenC0C("\\.\COM3", baud=9600, data=8, parity=no, stop=1) - OK
-     Connect("blynk-cloud.com", "8442") - OK
-     InOut() START
-     DSR is OFF
-   
+/***************************************************************
+* This part of the code was used in a test project under Windows and Arduino UNO with Luis Castillo and Yoimer Romï¿½n from NextWells company. Venezuela-USA.
+* Instrucctions:
+* - Get on Google Play Store or IOS Store and Download blynk. After installing, sign up.
+  - On your browser from you PC type http://docs.blynk.cc/. Check out the "Intro" section. Read this part carefully.
+  - We will need the blink libraries in our Arduino IDE in order stablish a connection with the Cloud Blynk Service. To do that; get
+     the lastest version of the libraries from here. https://github.com/blynkkk/blynk-library/releases/tag/v0.4.3 (Blynk_Release_v0.4.3.zip)
+     At the time this document was made, the lastest release was v0.4.3. Yours may vary.
+  - In case you've forgotten how to install libraries in Arduino IDE;
+     check this link on "Manual installation" https://www.arduino.cc/en/guide/libraries
+    After installing the Blink libraries, open an empty Scketch from Arduino IDE.
+    Copy and paste the ***NextWells-BLINK-ARDUINO UNO******* code Upload it to the Arduino UNO board.(This code is above)
+
+  - After succesfully uploading the code,extract the Blynk_Release_v0.4.3.zip files on C:\ directory.
+    This is for running the script which will make us get online with the Cloud Blynk Service.
+  - Run the blynk-ser.bat script. In order to do that , open cmd promt and get on this path C:\Blynk_Release_v0.4.3\libraries\Blynk\scripts>
+    Issue this command : blynk-ser.bat -c COM3 (In your case check your Arduino COM port) and press Enter, Enter , Enter (Yes, 3 times)
+    If there is Internet Connection and everything is OK, you should see something similar to this on the promt console:
+
+    Connecting device at COM3 to blynk-cloud.com:8442...
+    OpenC0C("\\.\COM3", baud=9600, data=8, parity=no, stop=1) - OK
+    Connect("blynk-cloud.com", "8442") - OK
+    InOut() START
+    DSR is OFF
+
 -   Now you should be able to play with the GPIO from the Blink app. For testing try Turning ON and Turning OFF the LED on PIN 13 using a botton.
 
- **************************************************************/
+**************************************************************/
 
 /***************************************NextWells-BLINK-ARDUINO UNO****************************************************/
 
@@ -59,14 +59,34 @@ SoftwareSerial DebugSerial(2, 3); // RX, TX
 
 #define BLYNK_PRINT DebugSerial
 #include <BlynkSimpleStream.h>
+#include <SimpleTimer.h>
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
 
 // Type here your AuthToken. Please keep the quotes.
-//It should be something like "369505e836ed435abe9c7e9db2db5471", not 369505e836ed435abe9c7e9db2db9090
+//It should be something like "369505e836ed435abe9c7e9db2db5471", not 369505e836ed435abe9c7e9db2db5471
 
-//char auth[] = "Your AuthToken";
+char auth[] = "369505e836ed435abe9c7e9db2db5471";
+
+float temp = 1.10; // Virtual Temperature
+
+SimpleTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  
+  temp += 0.25; // Increment the temperature by 0.25 Celsius degree
+  
+  //Blynk.virtualWrite(V5, millis() / 1000);
+  
+  Blynk.virtualWrite(V5, temp);
+}
 
 void setup()
 {
@@ -77,9 +97,24 @@ void setup()
   // Do not read or write this serial manually in your sketch
   Serial.begin(9600);
   Blynk.begin(Serial, auth);
+  
+  /* ////Enabling Virtual Pins
+  BLYNK_WRITE(); */
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
 }
+
+
+
+
 
 void loop()
 {
   Blynk.run();
+
+  /*//// Writing a string to virtual pin 1. This will be displayed on the Blynk App
+  Blynk.virtualWrite(1, "abcdeeee"); */
+  
+  timer.run(); // Initiates SimpleTimer
 }
